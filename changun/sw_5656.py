044b23f42a70c2b5849ee9673ce8cecc9420d026
창언 하 <giponge@gmail.com>
2019-01-15 02:57:16 +0900

    brick problem done

--- changun/sw_5656.py
+++ changun/sw_5656.py
@@ -1,33 +1,54 @@
-def Bomb(x,y,n,W,H,brick):
-    if x<0 or x>=W or y<0 or y>=H:
+import copy
+def Bomb(x,y,brick,pos):
+    W=len(brick)
+    if x<0 or x>=W :
         return
-    else:
-        if n==0:
-            return
-        elif n==1:  #1이면 한번 더 불러도 되는데 수행복잡도 때문에 조건 걸어줌
-            brick[x][y]=0
-            return
-        else:    
-            brick[x][y] = 0
-            for i in range(1-n,n):
-                DFS(x,y+i,brick[x][y+i],W,H)
-                DFS(x+i,y,brick[x+i][y],W,H)
-def DFS(pos,array):
+    H=len(brick[x])
+    if H==0:
+        return
+    if y<0 or y>=H:
+        return
+    n=brick[x][y]
+    if n==0:
+        return
+    elif n==1:  #1이면 한번 더 불러도 되는데 수행복잡도 때문에 조건 걸어줌
+        brick[x][y]=0
+        return
+    else:   
+        brick[x][y] = 0
+        for i in range(1-n,n):
+            Bomb(x,y+i,brick,pos)
+            Bomb(x+i,y,brick,pos)
+def DFS(n,start,brick,pos):
     global Minresult
-    if pos>2:
+    if pos>=n:
         count=0
-        for oneline in len(array):
-            for j in oneline:
-                count+=1
+        for oneline in brick:
+            count+=len(oneline)
         if count<Minresult:
             Minresult=count
     else:
-        for i in range(array):
+        x=start
+        y=len(brick[x])-1
+        Bomb(x,y,brick,pos)
+        i=0
+        while(i<=len(brick)-1):
+            j=0
+            while(j<=len(brick[i])-1):
+                if brick[i][j]==0:
+                    del brick[i][j]
+                    j-=1
+                j+=1
+            i+=1
+        # print("afterbrick",brick)
+        for i in range(len(brick)):
+            tmpbrick=copy.deepcopy(brick)
             pos+=1
-            Bomb()
-            DFS(pos)
+            DFS(n,i,tmpbrick,pos)
+            pos-=1
 casesize=int(input())
 for case in range(casesize):
+    Minresult=100000
     bricks=[]
     caseinfo = list(map(int,input().split()))
     for brick in range(caseinfo[1]):
@@ -36,21 +57,9 @@ for case in range(casesize):
         brick=list(map(int,input().split()))
         for j in range(len(brick)):
             if brick[j]!=0:
-                bricks[j].append(brick[j])      #열로 값을 전환하여 저장
-    print(bricks)
-    originalBricks = bricks.copy()
-    for i in range (caseinfo[1]):
-        for ball in rnage( [0]):
-            Hs=[len(bricks[height])-1 for height in len(bricks)] 
-            n=bricks[i][Hs[i]]
-            DFS(i,Hs[x],n,len(Hs),Hs[x]+1)
-            for aaa:    #터진것은 없애주자.
-                pass
-
-    # for ball in range(caseinfo[0]):     #공 떨어트리기 시작
-    #     Hs=[len(brick[height])-1 for height in len(bricks)]     
-    #     for x in range(len(Hs)):
-    #         n=brick[x][Hs[x]]
-    #         DFS(x,Hs[x],n,len(Hs),Hs[x]+1)
-    #         for i in range(caseinfo[1]):
-    #             pass
+                bricks[j]=[brick[j]]+bricks[j]
+                      #열로 값을 전환하여 저장
+    for i in range(len(bricks)):
+        runbricks = copy.deepcopy(bricks)
+        DFS(caseinfo[0],i,runbricks,0)
+    print('#',case+1,' ',Minresult,sep="")
